e9ebba00088d48d2672b6c1bbdae886855fdc776
Whatcott, Michael <mdwhatcott@gmail.com>
2016-02-24 12:35:59 -0700

    Rethought the whole thing. Provided getter methods.

--- struct_field.go
+++ struct_field.go
@@ -1,18 +1,18 @@
 package metrics
 
+import "sync/atomic"
+
 // Metrics can be used as a struct field and overridden with the Capture
 // function in unit test setups to allow assertions on counted and measured
 // values. This approach is similar to the one employed by the clock package
 // (see github.com/smartystreets/clock).
 type Metrics struct {
-	Counters map[CounterMetric]int64
-	Gauges   map[GaugeMetric]int64
+	All map[int]int64
 }
 
 func Capture() *Metrics {
 	return &Metrics{
-		Counters: make(map[CounterMetric]int64),
-		Gauges:   make(map[GaugeMetric]int64),
+		All: make(map[int]int64),
 	}
 }
 
@@ -20,30 +20,46 @@ func (this *Metrics) Count(id CounterMetric) bool {
 	if this == nil {
 		return standard.Count(id)
 	}
-	this.Counters[id]++
-	return true
+	return this.add(int(id), 1)
 }
 
 func (this *Metrics) CountN(id CounterMetric, increment int64) bool {
 	if this == nil {
 		return standard.CountN(id, increment)
 	}
-	this.Counters[id] += increment
-	return true
+	return this.add(int(id), increment)
 }
 
 func (this *Metrics) RawCount(id CounterMetric, value int64) bool {
 	if this == nil {
 		return standard.RawCount(id, value)
 	}
-	this.Counters[id] = value
-	return true
+	return this.set(int(id), value)
 }
 
 func (this *Metrics) Measure(id GaugeMetric, value int64) bool {
 	if this == nil {
 		return standard.Measure(id, value)
 	}
-	this.Gauges[id] = value
+	return this.set(int(id), value)
+}
+
+func (this *Metrics) add(id int, increment int64) bool {
+	atomic.AddInt64(&this.All[id], increment)
 	return true
+}
+
+func (this *Metrics) set(id int, value int64) bool {
+	atomic.StoreInt64(&this.All[id], value)
+	return true
+}
+
+// Helper function for test assertions:
+func (this *Metrics) CounterValue(id CounterMetric) int64 {
+	return atomic.LoadInt64(&this.All[int(id)])
+}
+
+// Helper function for test assertions:
+func (this *Metrics) GaugeValue(id GaugeMetric) int64 {
+	return atomic.LoadInt64(&this.All[int(id)])
 }
