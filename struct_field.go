3b3d55f95ab19aad6b369cdf1dc4f7df97862d19
Whatcott, Michael <mdwhatcott@gmail.com>
2016-04-20 16:49:43 -0600

    Reworked the struct field implementation. Now supports histograms too.

--- struct_field.go
+++ struct_field.go
@@ -1,70 +1,78 @@
 package metrics
 
-// TODO: add histogram support
+import "github.com/smartystreets/metrics/internal/hdrhistogram"
 
 // Metrics can be used as a struct field and overridden with the Capture
 // function in unit test setups to allow assertions on counted and measured
 // values. This approach is similar to the one employed by the clock package
 // (see github.com/smartystreets/clock).
 type Metrics struct {
-	all map[int]int64
+	Counters   map[CounterMetric]int64
+	Gauges     map[GaugeMetric]int64
+	Histograms map[HistogramMetric]*hdrhistogram.Histogram
 }
 
 func Capture() *Metrics {
 	return &Metrics{
-		all: make(map[int]int64),
+		Counters:   make(map[CounterMetric]int64),
+		Gauges:     make(map[GaugeMetric]int64),
+		Histograms: make(map[HistogramMetric]*hdrhistogram.Histogram),
 	}
 }
 
 func (this *Metrics) Count(id CounterMetric) bool {
-	if this == nil {
-		return standard.Count(id)
-	}
-	return this.add(int(id), 1)
+	return this.CountN(id, 1)
 }
 
 func (this *Metrics) CountN(id CounterMetric, increment int64) bool {
 	if this == nil {
 		return standard.CountN(id, increment)
 	}
-	return this.add(int(id), increment)
+	this.Counters[id] += increment
+	return true
 }
 
 func (this *Metrics) RawCount(id CounterMetric, value int64) bool {
 	if this == nil {
 		return standard.RawCount(id, value)
 	}
-	return this.set(int(id), value)
+	this.Counters[id] = value
+	return true
 }
 
 func (this *Metrics) Measure(id GaugeMetric, value int64) bool {
 	if this == nil {
 		return standard.Measure(id, value)
 	}
-	return this.set(int(id), value)
-}
-
-func (this *Metrics) add(id int, increment int64) bool {
-	this.all[id] += increment
+	this.Gauges[id] = value
 	return true
 }
 
-func (this *Metrics) set(id int, value int64) bool {
-	this.all[id] = value
-	return true
+func (this *Metrics) Record(id HistogramMetric, value int64) bool {
+	if this == nil {
+		return standard.Record(id, value)
+	}
+	histogram := this.Histograms[id]
+	if histogram == nil {
+		histogram = hdrhistogram.New(0, max, resolution)
+		this.Histograms[id] = histogram
+	}
+	return histogram.RecordValue(value) == nil
 }
 
 // Helper functions for test assertions:
 func (this *Metrics) CounterValue(id CounterMetric) int64 {
-	if this == nil {
-		return -1
-	}
-	return this.value(int(id))
+	return this.Counters[id]
 }
 func (this *Metrics) GaugeValue(id GaugeMetric) int64 {
-	if this == nil {
-		return -1
-	}
-	return this.value(int(id))
+	return this.Gauges[id]
 }
-func (this *Metrics) value(id int) int64 { return this.all[id] }
+func (this *Metrics) HistogramValue(id HistogramMetric) Histogram {
+	return this.Histograms[id]
+}
+
+const (
+	// int64 max value causes hdrhistogram to hang. 1 billion is probably high enough for most scenarios:
+	max        = 1000000000
+	resolution = 5
+)
--- struct_field_test.go
+++ struct_field_test.go
@@ -10,13 +10,14 @@ import (
 func TestMetricsAsStructField(t *testing.T) {
 	thing := &Thing{}
 	thing.metrics = Capture()
-	thing.Stuff()
+	thing.DoStuff()
 
 	assert := assertions.New(t)
-	assert.So(thing.metrics.CounterValue(1), should.Equal, 1)
-	assert.So(thing.metrics.CounterValue(2), should.Equal, 2)
-	assert.So(thing.metrics.CounterValue(3), should.Equal, 3)
-	assert.So(thing.metrics.CounterValue(4), should.Equal, 4)
+	assert.So(thing.metrics.CounterValue(0), should.Equal, 1)
+	assert.So(thing.metrics.CounterValue(1), should.Equal, 2)
+	assert.So(thing.metrics.CounterValue(2), should.Equal, 3)
+	assert.So(thing.metrics.GaugeValue(3), should.Equal, 4)
+	assert.So(thing.metrics.HistogramValue(4).Max(), should.Equal, 5)
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -25,9 +26,10 @@ type Thing struct {
 	metrics *Metrics
 }
 
-func (this *Thing) Stuff() {
-	this.metrics.Count(1)
-	this.metrics.CountN(2, 2)
-	this.metrics.RawCount(3, 3)
-	this.metrics.Measure(4, 4)
+func (this *Thing) DoStuff() {
+	this.metrics.RawCount(2, 3)
+	this.metrics.Measure(3, 4)
+	this.metrics.Record(4, 5)
+	this.metrics.Count(0)
+	this.metrics.CountN(1, 2)
 }
