919c4377422b2c02481812228b10f0c0f379b057
Michael Whatcott <mdwhatcott@gmail.com>
2021-02-19 10:53:45 -0700

    Testing metric values explicitly (not just through exporter 'UI').

--- metrics_test.go
+++ metrics_test.go
@@ -7,53 +7,46 @@ import (
 	"testing"
 )
 
-func TestMetricsRendering(t *testing.T) {
-	counter := NewCounter("my_counter",
-		Options.Description("counter description"),
-	)
-	counterWithLabels := NewCounter("my_counter_with_labels",
-		Options.Description("counter description"),
-		Options.Label("counter_label_key", "counter_label_value"),
-	)
-	gauge := NewGauge("my_gauge",
-		Options.Description("gauge description"),
-	)
-	gaugeWithLabels := NewGauge("my_gauge_with_labels",
-		Options.Description("gauge description"),
-		Options.Label("gauge_label_key", "gauge_label_value"),
-	)
+func TestMetricsValues(t *testing.T) {
+	metrics := NewTestMetrics()
 
-	counter.Increment()
-	counterWithLabels.IncrementN(2)
-	gauge.Increment()
-	gauge.IncrementN(2)
-	gaugeWithLabels.Measure(4)
+	metrics.counter1.Increment()
+	metrics.counter2.IncrementN(2)
+	metrics.gauge1.Increment()
+	metrics.gauge1.IncrementN(2)
+	metrics.gauge2.Measure(4)
+
+	assertEqual(t, int64(1), metrics.counter1.Value())
+	assertEqual(t, int64(2), metrics.counter2.Value())
+	assertEqual(t, int64(3), metrics.gauge1.Value())
+	assertEqual(t, int64(4), metrics.gauge2.Value())
+}
+
+func TestMetricsRendering(t *testing.T) {
+	metrics := NewTestMetrics()
+
+	metrics.counter1.IncrementN(1)
+	metrics.counter2.IncrementN(2)
+	metrics.gauge1.IncrementN(3)
+	metrics.gauge2.Measure(4)
 
 	exporter := NewExporter()
-	exporter.Add(counter, counterWithLabels, gauge, gaugeWithLabels)
+	exporter.Add(
+		metrics.counter1,
+		metrics.counter2,
+		metrics.gauge1,
+		metrics.gauge2,
+	)
 	recorder := httptest.NewRecorder()
 
 	exporter.ServeHTTP(recorder, nil)
 
 	actualBody := strings.TrimSpace(recorder.Body.String())
 
-	assertEqual(t, expectedBody, actualBody)
+	assertEqual(t, expectedExporterBody, actualBody)
 }
 
-func assertEqual(t *testing.T, expected, actual interface{}) {
-	if reflect.DeepEqual(expected, actual) {
-		return
-	}
-	t.Helper()
-	t.Errorf("\n"+
-		"Expected: [%v]\n"+
-		"Actual:   [%v]",
-		expected,
-		actual,
-	)
-}
-
-var expectedBody = strings.TrimSpace(`
+var expectedExporterBody = strings.TrimSpace(`
 # HELP my_counter counter description
 # TYPE my_counter counter
 my_counter 1
@@ -70,3 +63,47 @@ my_gauge 3
 # TYPE my_gauge_with_labels gauge
 my_gauge_with_labels{ gauge_label_key="gauge_label_value" } 4
 `)
+
+type TestMetrics struct {
+	counter1 Counter
+	counter2 Counter
+	gauge1   Gauge
+	gauge2   Gauge
+}
+
+func NewTestMetrics() *TestMetrics {
+	counter1 := NewCounter("my_counter",
+		Options.Description("counter description"),
+	)
+	counter2 := NewCounter("my_counter_with_labels",
+		Options.Description("counter description"),
+		Options.Label("counter_label_key", "counter_label_value"),
+	)
+	gauge1 := NewGauge("my_gauge",
+		Options.Description("gauge description"),
+	)
+	gauge2 := NewGauge("my_gauge_with_labels",
+		Options.Description("gauge description"),
+		Options.Label("gauge_label_key", "gauge_label_value"),
+	)
+
+	return &TestMetrics{
+		counter1: counter1,
+		counter2: counter2,
+		gauge1:   gauge1,
+		gauge2:   gauge2,
+	}
+}
+
+func assertEqual(t *testing.T, expected, actual interface{}) {
+	if reflect.DeepEqual(expected, actual) {
+		return
+	}
+	t.Helper()
+	t.Errorf("\n"+
+		"Expected: [%v]\n"+
+		"Actual:   [%v]",
+		expected,
+		actual,
+	)
+}
