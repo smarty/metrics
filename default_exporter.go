c7533fcf06fe655cc6f8b7d2451f6e1ca4c4be5d
Dirk Whatcott <dirk@smartystreets.com>
2021-03-24 15:24:16 -0600

    Fixed the build and enhanced the ServeHTTP function.

--- default_exporter.go
+++ default_exporter.go
@@ -20,15 +20,16 @@ func (this *defaultExporter) Add(items ...Metric) {
 func (this *defaultExporter) ServeHTTP(response http.ResponseWriter, _ *http.Request) {
 	response.Header().Set("Content-Type", "text/plain; version=0.0.4")
 	for _, item := range this.metrics {
-		_, _ = fmt.Fprintf(response, outputFormat,
-			item.Name(), item.Description(),
-			item.Name(), item.Type(),
-			item.Name(), item.Labels(), item.Value()) // TODO: Accept multiple label key-pairs
+		_, _ = fmt.Fprintf(response, outputFormatHelp, item.Name(), item.Description())
+		_, _ = fmt.Fprintf(response, outputFormatType, item.Name(), item.Type())
+		if item.Type() == "Histogram" {
+			_, _ = fmt.Fprintf(response, outputFormatMetric, item.Name(), item.(Histogram).Buckets(), item.Value())
+		} else {
+			_, _ = fmt.Fprintf(response, outputFormatMetric, item.Name(), item.Labels(), item.Value()) // TODO: Accept multiple label key-pairs
+		}
 	}
 }
 
-const outputFormat = `
-# HELP %s %s
-# TYPE %s %s
-%s%s %d
-`
+const outputFormatHelp = "\n# HELP %s %s\n"
+const outputFormatType = "# TYPE %s %s\n"
+const outputFormatMetric = "%s%s %d\n"
--- metrics_test.go
+++ metrics_test.go
@@ -69,7 +69,7 @@ my_gauge_with_labels{ gauge_label_key="gauge_label_value" } 4
 
 # HELP my_histogram_with_buckets histogram description
 # TYPE my_histogram_with_buckets histogram
-my_histogram_with_buckets{ le="50.000", histogram_key1="histogram_value1" } 6
+my_histogram_with_buckets{ histogram_key1="histogram_value1" } 6
 `)
 
 type TestMetrics struct {
