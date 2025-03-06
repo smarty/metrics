ca82e6e2e7b00a85b2836e15ca805cf455013e55
Michael Whatcott <mike@smarty.com>
2025-03-06 14:52:24 -0700

    A simple alerting monitor.
    Maybe too simple..

---
+++ alerting/alerting.go
@@ -0,0 +1,59 @@
+package alerting
+
+import (
+	"fmt"
+
+	"github.com/smarty/metrics/v2"
+)
+
+type Monitor interface {
+	Alert(severity)
+}
+
+type severity int
+
+const (
+	Anomaly  severity = 1
+	Failure  severity = 2
+	Disaster severity = 3
+)
+
+func (this severity) String() string {
+	switch this {
+	case Anomaly:
+		return "Anomaly"
+	case Failure:
+		return "Failure"
+	case Disaster:
+		return "Disaster"
+	default:
+		return fmt.Sprintf("Unknown(%d)", this)
+	}
+}
+
+//////////////////////////////////////////////////////////////////////////////////
+
+type metricsMonitor struct {
+	anomalies metrics.Counter
+	failures  metrics.Counter
+	disasters metrics.Counter
+}
+
+func NewMetricsMonitor(anomalies, failures, disasters metrics.Counter) Monitor {
+	return &metricsMonitor{
+		anomalies: anomalies,
+		failures:  failures,
+		disasters: disasters,
+	}
+}
+
+func (this *metricsMonitor) Alert(severity severity) {
+	switch severity {
+	case Anomaly:
+		this.anomalies.Increment()
+	case Failure:
+		this.failures.Increment()
+	case Disaster:
+		this.disasters.Increment()
+	}
+}
